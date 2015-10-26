ef65fa50694d9de0ccccecdb2cb1596f26c080b8
ma278221 <mayur.popade@wipro.com>
2015-10-26 17:06:54 +0530

    TestClaim added.

---
+++ Test2/src/com/wipro/check/TestClaim.java
@@ -0,0 +1,14 @@
+package com.wipro.check;
+
+import java.util.Scanner;
+
+public class TestClaim 
+{
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	
+	public static void main(String args[])
+	{
+		
+	}
+}
